a8ce4c4d8079dd107fc17efa983c279c9c0df631
juanjo96dev <juanjo96developer@gmail.com>
2023-02-18 16:35:16 +0100

    wip

--- tests/config/orm.config.ts
+++ tests/config/orm.config.ts
@@ -3,7 +3,6 @@ import { Simple } from "../entities";
 
 const dataBaseSource = new DataSource({
   type: (process.env.DATABASE_TYPE as any) || "mysql",
-  port: Number(process.env.DB_PORT) || 3306,
   host: "localhost",
   username: String(process.env.DB_USER) || "root",
   password: String(process.env.DB_PASSWORD) || "password",
