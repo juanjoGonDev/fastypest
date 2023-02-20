6b1496ebc63c8bac0b5c03c81413e0df1abc9f39
Juan José González Rodríguez <50484296+juanjo96Dev@users.noreply.github.com>
2023-02-20 07:49:38 +0100

    Refactor

--- tests/seeds/simple.seed.ts
+++ tests/seeds/simple.seed.ts
@@ -2,7 +2,7 @@ import { Simple } from "../entities";
 
 export const simple: Partial<Simple>[] = [];
 
-export const seedCount = process.env.Seed_COUNT || 5_000;
+export const seedCount = process.env.SEED_COUNT || 5_000;
 
 for (let x = 1; x <= seedCount; x++) {
   simple.push({
