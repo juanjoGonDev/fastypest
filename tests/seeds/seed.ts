03aae09b6799b6dda75d4c4567b65bb848b8543e
Juan José González Rodríguez <50484296+juanjo96Dev@users.noreply.github.com>
2023-02-20 07:52:13 +0100

    Refactor

--- tests/seeds/seed.ts
+++ tests/seeds/seed.ts
@@ -3,7 +3,9 @@ import { Simple } from "../entities";
 import { simple } from "./simple.seed";
 
 export const seed = async (connection: DataSource) => {
-  const simpleRepository = connection.manager.getRepository(Simple);
-  await simpleRepository.clear();
-  await simpleRepository.insert(simple);
+  await connection.manager.transaction(async em => {
+    const simpleRepository = em.getRepository(Simple);
+    await simpleRepository.clear();
+    await simpleRepository.insert(simple);
+  });
 };
