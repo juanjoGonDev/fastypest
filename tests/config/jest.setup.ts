5f269db362cdf856baa62d3ae5479aa96c66b206
juanjo96dev <juanjo96developer@gmail.com>
2023-02-22 22:16:23 +0100

    wip

--- tests/config/jest.setup.ts
+++ tests/config/jest.setup.ts
@@ -3,6 +3,8 @@ import { Fastypest } from "../../src/index";
 import { seed } from "../seeds/seed";
 import { initialize } from "./orm.config";
 
+jest.setTimeout(20_000);
+
 let fastypest: Fastypest;
 let connection: DataSource;
 
