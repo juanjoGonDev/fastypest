f7ced4c6156dfc2c5c00af4940506214e8cff81a
juanjo96dev <juanjo96developer@gmail.com>
2023-02-27 12:37:15 +0100

    fix: reset auto-increment postgres

--- tests/seeds/seed.ts
+++ tests/seeds/seed.ts
@@ -4,8 +4,19 @@ import { simple } from "./simple.seed";
 
 export const seed = async (connection: DataSource) => {
   await connection.manager.transaction(async (em) => {
-    const simpleRepository = em.getRepository(Simple);
-    await simpleRepository.clear();
-    await simpleRepository.insert(simple);
+    const repository = em.getRepository(Simple);
+    await em.query(truncate(connection, repository.metadata.tableName));
+    await repository.insert(simple);
   });
+};
+
+const truncate = (connection: DataSource, tableName: string) => {
+  const dbType = connection.options.type;
+  const resetID: Record<string, string> = {
+    postgres: `TRUNCATE TABLE ${tableName} RESTART IDENTITY`,
+    mysql: `TRUNCATE TABLE ${tableName}`,
+    mariadb: `TRUNCATE TABLE ${tableName}`,
+  };
+
+  return resetID[dbType];
 };
