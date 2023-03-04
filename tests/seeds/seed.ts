38ab234d9a61155f8c23080aa70d98273125a5f5
juanjo96dev <juanjo96developer@gmail.com>
2023-03-04 22:52:17 +0100

    feat: cockroachdb supported

--- tests/seeds/seed.ts
+++ tests/seeds/seed.ts
@@ -1,22 +1,37 @@
-import { DataSource } from "typeorm";
+import {
+  DataSource,
+  DataSourceOptions,
+  EntityManager,
+  EntityTarget,
+} from "typeorm";
+import { SQLScript } from "../../src/core/sql-script";
 import { Simple } from "../entities";
 import { simple } from "./simple.seed";
 
 export const seed = async (connection: DataSource) => {
+  const seed = new Seed(connection.options.type);
   await connection.manager.transaction(async (em) => {
-    const repository = em.getRepository(Simple);
-    await em.query(truncate(connection, repository.metadata.tableName));
-    await repository.insert(simple);
+    await seed.seed(em, Simple, simple);
   });
 };
 
-const truncate = (connection: DataSource, tableName: string) => {
-  const dbType = connection.options.type;
-  const resetID: Record<string, string> = {
-    postgres: `TRUNCATE TABLE ${tableName} RESTART IDENTITY`,
-    mysql: `TRUNCATE TABLE ${tableName}`,
-    mariadb: `TRUNCATE TABLE ${tableName}`,
-  };
+class Seed extends SQLScript {
+  constructor(dbType: DataSourceOptions["type"]) {
+    super(dbType);
+  }
 
-  return resetID[dbType];
-};
+  async seed(em: EntityManager, target: EntityTarget<any>, data: object[]) {
+    const repository = em.getRepository(target);
+    const queries = {
+      truncate: this.getQuery("truncateTable", {
+        tableName: repository.metadata.tableName,
+      }),
+      fk_enable: this.getQuery("foreignKey.enable"),
+      fk_disable: this.getQuery("foreignKey.disable"),
+    };
+    await em.query(queries.fk_disable);
+    await em.query(queries.truncate);
+    await repository.insert(data);
+    await em.query(queries.fk_disable);
+  }
+}
